90e2062a52e27754a22a21455619a9ac3b35ba25
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-08 16:36:48 +0100

    Structure of page done

--- lib/ui/pages/ExchangePage.dart
+++ lib/ui/pages/ExchangePage.dart
@@ -1,27 +1,8 @@
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:easy_exchange/util/url.dart';
-import 'package:easy_exchange/model/currency_rate.dart';
+import 'package:easy_exchange/model/index.dart';
+import 'package:easy_exchange/ui/widget/index.dart';
 
-
-Future<CurrencyRate> fetchCurrencyRate(http.Client client) async {
-  
-  final response = await client.get(Url.exchangeBaseUrl);
-
-  if (response.statusCode == 200) {
-    final parsed = json.decode(response.body);
-
-    return CurrencyRate.fromJson(parsed);
-  } else {
-    // If the server did not return a 200 OK response, then throw an exception.
-    throw Exception('Failed to load album');
-  }
-
-}
 
 class ExchangePage extends StatefulWidget {
   @override
@@ -35,41 +16,41 @@ class _ExchangePage extends State<ExchangePage> {
         appBar: AppBar(
           title: Text('Easy Exchange'),
         ),
-        body: FutureBuilder<CurrencyRate>(
-            future: fetchCurrencyRate(http.Client()),
-            builder: (context, snapshot) {
-              if (snapshot.hasError)
-                print(snapshot.error);
-              else
-                print("check");
-
-              return snapshot.hasData
-                  ? CurrencyRateList(currencyRates: snapshot.data)
-                  : Center(child: CircularProgressIndicator());
-            }));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CurrencyButton(),
+                MoneyInput()
+              ],
+            ),
+            SizedBox(height: 20.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('CHANGE'),
+                  padding: EdgeInsets.all(15.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)),
+                  color: Colors.white,
+                  textColor: Colors.black,
+                  onPressed: () {},
+                )
+              ],
+            ),
+            SizedBox(height: 20.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CurrencyButton(),
+                MoneyInput()
+              ],
+            )
+          ],
+        ));
   }
 }
 
-class CurrencyRateList extends StatelessWidget {
-  final CurrencyRate currencyRates;
-
-  CurrencyRateList({Key key, this.currencyRates}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-      ),
-      itemCount: currencyRates.rates.length,
-      itemBuilder: (context, index) {
-        return Row(children: <Widget>[
-          Text(currencyRates.rates[index].currency.name),
-          Text(currencyRates.rates[index].currency.code),
-          Text(currencyRates.rates[index].rate.toString()),
-        ],
-        ); 
-      },
-    );
-  }
-}
