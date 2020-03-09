9f2a4d55b9b82dc6c5fc281aa3d3cca512604d4c
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-09 19:12:24 +0100

    widgets modified to adjust new api functionality

--- lib/ui/widget/index.dart
+++ lib/ui/widget/index.dart
@@ -1,3 +1,4 @@
 export 'package:easy_exchange/ui/widget/money_input.dart';
-export 'package:easy_exchange/ui/widget/currencyrate_button.dart';
+export 'package:easy_exchange/ui/widget/currency_rates_button_widget.dart';
+export 'package:easy_exchange/ui/widget/currency_rates_list_widget.dart';
 
